2311ffd2166e7d7a2ca52e3d5b5a85cf33baaf8a
soniyasunny098 <77632291+soniyasunny098@users.noreply.github.com>
2021-04-15 11:59:52 +0530

    Update stream_strokedata (1).py

--- stream_strokedata (1).py
+++ stream_strokedata (1).py
@@ -17,8 +17,8 @@ def predict(model, input_df):
 
 def run():
     from PIL import Image
-    image = Image.open('/content/brainomixstroke.jpg')
-    image_office = Image.open('/content/stroke-recovery-timeline.jpg')
+    image = Image.open('brainomixstroke.jpg')
+    image_office = Image.open('stroke-recovery-timeline.jpg')
     st.image(image,use_column_width=True)
     add_selectbox = st.sidebar.selectbox(
     "How would you like to predict?",
